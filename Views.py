28e167e05f2815cb3b669fa43006c225bf810554
fmercier <72445125+fmercier1@users.noreply.github.com>
2020-11-05 14:55:58 +0100

    Delete Views.py

--- Views.py
+++
@@ -1,8 +0,0 @@
-def login(request):
-       context = RequestContext(request)
-       return render_to_response("templates/login.html", context)    
-
-
-def all_members(request): <--
-       return render(request, 'templates/members.html', 
-{'members': Member.objects.all()})
